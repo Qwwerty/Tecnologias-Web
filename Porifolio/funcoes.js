714473835591cbb4c032fbd9a31729ff6c3105cd
qwwerty <verdinhoraro2009@hotmail.com>
2019-06-14 18:48:27 -0300

    GOOD

---
+++ Porifolio/funcoes.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+	$("#menu #ex1").click(function(){
+		$("#resposta").load("Exemplo1.html");
+	});
+	$("#menu #ex2").click(function(){
+		$("#resposta").load("Exemplo2.html");
+	});
+	$("#menu #ex3").click(function(){
+		$("#resposta").load("Exemplo3.html");
+	});
+	$("#menu #ex4").click(function(){
+		$("#resposta").load("Exemplo4.html");
+	});
+	$("#menu #ex5").click(function(){
+		$("#resposta").load("Exemplo5.html");
+	});
+	$("#menu #ex6").click(function(){
+		$("#resposta").load("Exemplo6.html");
+	});
+	$("#menu #ex7").click(function(){
+		$("#resposta").load("Exemplo7.html");
+	});
+	$("#menu #ex8").click(function(){
+		$("#resposta").load("Exemplo8.html");
+	});
+	$("#menu #ex9").click(function(){
+		$("#resposta").load("Exemplo9.html");
+	});
+	$("#menu #ex10").click(function(){
+		$("#resposta").load("Exemplo10.html");
+	});
+	$("#menu #ex11").click(function(){
+		$("#resposta").load("Exemplo11.html");
+	});
+	$("#menu #ex12").click(function(){
+		$("#resposta").load("Exemplo12.html");
+	});
+	$("#menu #ex13").click(function(){
+		$("#resposta").load("Exemplo13.html");
+	});
+	$("#menu #ex14").click(function(){
+		$("#resposta").load("Exemplo14.html");
+	});
+	$("#menu #ex15").click(function(){
+		$("#resposta").load("Exemplo15.html");
+	});
+	$("#menu #ex16").click(function(){
+		$("#resposta").load("Exemplo16.html");
+	});
+	$("#menu #ex17").click(function(){
+		$("#resposta").load("Exemplo17.html");
+	});
+});	
